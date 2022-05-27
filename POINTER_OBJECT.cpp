5bc17c34cfe07395d602a69826205c9c5c30c280
DHIRAJ DARAKHE <89730354+dhirajdarakhe@users.noreply.github.com>
2022-05-27 16:25:05 +0530

    POINTER_OBJECT

---
+++ POINTER_OBJECT.cpp
@@ -0,0 +1,36 @@
+// DHIRAJ LAXMAN DARAKHE
+// 21118 (com)
+// PICT, PUNE
+
+
+#include <bits/stdc++.h>
+using namespace std;
+class m{
+    int r;
+    int p;
+    public:
+   
+    m()
+    { 
+        r = p =-1;
+        cout<<"contructor called";
+    }
+};
+int main()
+{
+    m *b;
+
+  return 0;
+}
+
+
+
+
+/// pointer object doesn't call its contructor
+
+//  output 
+//  nothing will be printed
+
+
+///  when we create the ponter object 
+///  we have to explicitly call the contructor , methos, destructor ;
